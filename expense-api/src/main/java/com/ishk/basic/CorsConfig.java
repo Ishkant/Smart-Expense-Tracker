e84f5a43cd7606e7d6879b57d3713462661ebc3c
Ishkant <ishkanttyagi@gmail.com>
2025-07-26 21:27:52 +0530

    Update CorsConfig.java

--- expense-api/src/main/java/com/ishk/basic/CorsConfig.java
+++ expense-api/src/main/java/com/ishk/basic/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://smart-expense-tracker.vercel.app")
+                        .allowedOrigins("https://smart-expense-tracker-snowy.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
